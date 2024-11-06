29b0cd89198562ce2f08dd8d177dc1769d75004f
YusufBalmumcu <162025366+YusufBalmumcu@users.noreply.github.com>
2024-11-06 18:21:25 +0300

    Add files via upload

---
+++ week5/spawnmanagerscript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanagerscript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyprefab;
+
+    [SerializeField]
+    GameObject enemycontainer;
+
+    bool stopspawning = false;
+
+    public void OnPlayerDeath()
+    {
+        stopspawning = true;
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (stopspawning == false)
+        {
+            Vector3 position = new Vector3(Random.Range(-7.8f, 7.8f), 6.5f, 1.0f);
+            GameObject new_enemy = Instantiate(enemyprefab, position, Quaternion.identity);
+            new_enemy.transform.parent = enemycontainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    
+    void Start()
+    {
+        
+        StartCoroutine(SpawnRoutine());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
